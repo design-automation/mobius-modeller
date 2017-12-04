b58267f8e326e97315a2b0fdb9c260198d100c42
Akshata Mohanty <akshatamohanty@gmail.com>
2017-12-04 19:49:32 +0930

    package.json updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,28 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  nodes = [
-    {
-      id: 1,
-      name: 'root1',
-      children: [
-        { id: 2, name: 'child1' },
-        { id: 3, name: 'child2' }
-      ]
-    },
-    {
-      id: 4,
-      name: 'root2',
-      children: [
-        { id: 5, name: 'child2.1' },
-        {
-          id: 6,
-          name: 'child2.2',
-          children: [
-            { id: 7, name: 'subsub' }
-          ]
-        }
-      ]
+    action = {
+        a1s: 25,
+        a2s: 50,
+        a3s: 25,
+        a1v: true,
+        a2v: false,
+        a3v: true,
+        useTransition: true,
     }
-  ];
+
+    update(){
+      this.action.a2v = false;
+    }
 }
