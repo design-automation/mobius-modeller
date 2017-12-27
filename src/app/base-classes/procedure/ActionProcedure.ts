8a0eb15aa80a520d23dfd1cf0e66556046c5e638
Akshata Mohanty <akshatamohanty@gmail.com>
2017-12-27 12:10:37 +0800

    procedure item bug fixed

--- src/app/base-classes/procedure/ActionProcedure.ts
+++ src/app/base-classes/procedure/ActionProcedure.ts
@@ -4,12 +4,14 @@ import {IComponent} from "./IComponent";
 
 export class ActionProcedure extends Procedure{
 
-	constructor(data ?: {result: string, module: string, function: string, params: string[]}){
+	constructor(data ?: {result: string, module: string, function: string, params: [{type: string, value: string}] }){
 		super(ProcedureTypes.Action, false); 
 
 		if(data == undefined){
 			data = {result: undefined, module: undefined, function: undefined, params: []} 
 		}
+
+		console.log(data.params);
 
 		let left: IComponent = { expression: data.result, 
 								 isAction: false, 
@@ -19,11 +21,11 @@ export class ActionProcedure extends Procedure{
 								 params: undefined
 								}
 		let right: IComponent = { expression: data.module + /*"." + data.category +*/ "::" + data.function, 
-								 isAction: true, 
-								 module: data.module, 
-								 category: undefined,//data.category, 
-								 fn_name: data.function,
-								 params: data.params
+								  isAction: true, 
+								  module: data.module, 
+								  category: undefined,//data.category, 
+								  fn_name: data.function,
+								  params: data.params.map(function(p){ return { type: p.type, value: p.value }; })
 								}
 
 		if(data.result !== undefined){
