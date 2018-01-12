dda7adf8ef8a98459239d3b1c4349fe5f94c8977
Akshata Mohanty <akshatamohanty@gmail.com>
2018-01-12 12:52:49 +0800

    help docs from json

--- src/app/ui-components/viewers/help-viewer/help-viewer.component.ts
+++ src/app/ui-components/viewers/help-viewer/help-viewer.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { LayoutService } from '../../../global-services/layout.service'; 
+import { FlowchartService } from '../../../global-services/flowchart.service'; 
 import { Subscription } from 'rxjs/Subscription';
 import { DomSanitizer} from '@angular/platform-browser';
+
+import docs from '../../../../../node_modules/gs-modelling/docs_json/gs-modelling.json';
 
 @Component({
   selector: 'app-help-viewer',
@@ -13,20 +16,41 @@ export class HelpViewerComponent implements OnInit {
   _url;
   private _subscription: Subscription;
 
+  _helpMods: any;
+
   //modules/_group_.html
-  constructor(private layoutService: LayoutService, private sanitizer: DomSanitizer) { 
+  constructor(private layoutService: LayoutService, private sanitizer: DomSanitizer, private flowchartService: FlowchartService) { 
   		this.sanitizer = sanitizer;
   		this._subscription = this.layoutService.getMessage().subscribe(message => { 
           if(message.text.startsWith("Module: ")){
   			    this.notify();
           }
   		});
+
+      let mods = this.flowchartService.getModules().map(function(m){
+          return m["_name"].toLowerCase();
+      });
+
+      this._helpMods = docs.children.filter(function(child){
+          let mod_name: string = child.name.substring(1, child.name.length - 1);
+          return mods.indexOf(mod_name) > -1;
+      })
+
+      console.log(this._helpMods);
   }
 
   notify(): void{
   		let url_segment: string = this.layoutService.getUrl();
       let url: string = 'https://phtj.github.io/gs-modelling/docs/' + url_segment;
       this._url = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  getUrl(name: string, fn: string): string{
+    return "_" + name.substring(1, name.length - 1).toLowerCase() + "_.html#" + fn.toLowerCase(); 
+  }
+
+  getModName(name: string): string{
+    return name.substring(1, name.length - 1).toUpperCase();
   }
 
   ngOnInit() { 
