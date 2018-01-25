96d30721a4daa77a0b89915e3d858fbcbf5e6913
Akshata Mohanty <akshatamohanty@gmail.com>
2018-01-25 16:07:44 +0800

    cheat for gs-model-data

--- src/app/base-classes/flowchart/Flowchart.ts
+++ src/app/base-classes/flowchart/Flowchart.ts
@@ -373,20 +373,20 @@ export class Flowchart implements IFlowchart{
 			let outputPort = node.getOutputByIndex(edge.output_address[1]);
 			let inputPort = inputNode.getInputByIndex(edge.input_address[1]);
 
+			inputPort.setComputedValue( outputPort.getValue() );
 
 			let outVal = outputPort.getValue();
-			// var bookmark = vc.getVersion();
-			// vc.gotoVersion(bookmark);
 			if(outVal.constructor.name == "Model"){
+				console.log("original model: ", outVal);
 				let modelData: gs.IModelData = outVal.toJSON();
-				let model = new gs.Model(modelData);
-				console.log(model === outVal);
-				inputPort.setComputedValue( model );
-			}
-			else{
-				inputPort.setComputedValue( outputPort.getValue() );
-			}
+        		let model = new gs.Model(modelData);
+        		model["_kernel"]._objs = JSON.parse(JSON.stringify(outVal["_kernel"]._objs));
+        		model["_kernel"]._points = JSON.parse(JSON.stringify(outVal["_kernel"]._points));
+				console.log("new model: ", model);
 
+				outputPort.setComputedValue( model );
+			}
+			
 
 
 			// let value = outputPort.getValue();
