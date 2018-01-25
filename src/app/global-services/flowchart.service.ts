02fab5acf69ec93d22a27f801e1cc3ae9421acdd
Akshata Mohanty <akshatamohanty@gmail.com>
2018-01-26 00:29:08 +0800

    calc module fixed

--- src/app/global-services/flowchart.service.ts
+++ src/app/global-services/flowchart.service.ts
@@ -270,6 +270,7 @@ export class FlowchartService {
                         {_name: "Plane", _version: 0.1, _author: "Patrick"},
                         {_name: "Split", _version: 0.1, _author: "Patrick"},
                         {_name: "Intersect", _version: 0.1, _author: "Patrick"},
+                        {_name: "Calc", _version: 0.1, _author: "Patrick"},
                         {_name: "Examples", _version: 0.1, _author: "Patrick"}
                         //{_name: "Calc", _version: 0.1, _author: "Patrick"}
                       ]
