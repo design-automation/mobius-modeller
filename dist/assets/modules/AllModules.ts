78bfd7f007c65dc1b0271d3af6e57ce4ce7a4193
Akshata Mohanty <akshatamohanty@gmail.com>
2018-02-05 16:05:49 +0800

    week-2-dist

--- dist/assets/modules/AllModules.ts
+++ dist/assets/modules/AllModules.ts
@@ -21,6 +21,5 @@ let Topo: IModule = ModuleUtils.createModule("Topo", GSS.topo);
 let List: IModule = ModuleUtils.createModule("List", GSS.list);
 let Math: IModule = ModuleUtils.createModule("Math", GSS.math);
 let String: IModule = ModuleUtils.createModule("String", GSS.string);
-let Calc: IModule = ModuleUtils.createModule("Calc", GSS.calc);
 
-export {Calc,List, Math, String, Circle, Model, Attrib, Examples, Intersect, Obj, Plane, Pline, PMesh, Point, Query, Ray, Split, Topo};
+export {Attrib, Examples, Intersect, Model, Obj, Plane, Pline, PMesh, Point, Query, Ray, Split, Topo, List, Math, String};
