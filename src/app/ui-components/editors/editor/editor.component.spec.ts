520440010c028bce3ae9f3d34556cd33575dab12
Akshata Mohanty <akshatamohanty@gmail.com>
2018-03-22 17:22:31 +0800

    jasmine tests deactivated

--- src/app/ui-components/editors/editor/editor.component.spec.ts
+++ src/app/ui-components/editors/editor/editor.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+/*import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditorComponent } from './editor.component';
 
@@ -23,3 +23,4 @@ describe('EditorComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+*/
